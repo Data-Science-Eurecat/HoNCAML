fd3770fbda63dabebb060d4d5987fbbb2524c643
Xavier de Juan <xavier.dejuan@eurecat.org>
2022-08-02 16:26:45 +0200

    unify extract and load operations

--- src/data/extract.py
+++ src/data/extract.py
@@ -2,6 +2,7 @@ from typing import Dict, Tuple
 import pandas as pd
 import os
 import yaml
+import joblib
 
 
 def read_yaml(file_path: str) -> Dict:
@@ -21,7 +22,7 @@ def read_yaml(file_path: str) -> Dict:
     return params
 
 
-def read_data(settings: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def read_dataframe(settings: Dict) -> pd.DataFrame:
     """
     Read data from disk using specified settings.
 
@@ -29,8 +30,7 @@ def read_data(settings: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
         settings (Dict): Params used for input data extraction.
 
     Returns:
-        dataset, target (Tuple[pd.DataFrame, pd.DataFrame]): the dataset and
-        the target column.
+        df_datatype (pd.DataFrame): the dataset as pandas dataframe.
     """
     filepath = os.path.join(settings['path'], settings['data'])
     extension = settings['data'].split('.')[-1].lower()
@@ -40,6 +40,17 @@ def read_data(settings: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
         df_datatype = pd.read_excel(filepath)
     else:
         raise Exception(f'File extension {extension} not recognized')
-    dataset = df_datatype.drop(settings['target'], axis=1)
-    target = df_datatype[[settings['target']]]
-    return dataset, target
+    return df_datatype
+
+
+def read_model(settings: Dict) -> object:
+    """
+    Load a trained model from a given path.
+    Args:
+        settings (Dict): the settings containing the path to the file.
+    Returns:
+        model (object): The read model from disk.
+    """
+    filepath = os.path.join(settings['path'], settings['file'])
+    model = joblib.load(filepath)
+    return model
--- src/data/load.py
+++ src/data/load.py
@@ -1,24 +1,27 @@
 from typing import Dict
 import pandas as pd
 import os
+import joblib
 
 
-def save_data(dataset: pd.DataFrame, target: pd.DataFrame, settings: Dict)\
-        -> None:
+def save_dataframe(dataset: pd.DataFrame, settings: Dict) -> None:
     """
     Save the dataset and target columns to disk using the settings given.
 
     Args:
         dataset (pd.DataFrame): the dataset.
-        dataset (pd.DataFrame): the target column.
         settings (Dict): Params used for data processing.
     """
-    df = pd.concat((dataset, target), axis=1)
     filepath = os.path.join(settings['path'], settings['data'])
     extension = settings['data'].split('.')[-1].lower()
     if extension == 'csv':
-        df.to_csv(filepath, index=False)
+        dataset.to_csv(filepath, index=False)
     elif extension in ['xlsx', 'xls']:
-        df.to_excel(filepath, index=False)
+        dataset.to_excel(filepath, index=False)
     else:
         raise Exception(f'File extension {extension} not recognized')
+
+
+def save_model(model: object, settings: Dict) -> None:
+    filepath = os.path.join(settings['path'], settings['file'])
+    joblib.dump(model, filepath)
