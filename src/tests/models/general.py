dee1e02dc16b8e73a742de836eaef5b3fbb5c1a1
Xavier de Juan <xavier.dejuan@eurecat.org>
2022-09-21 16:04:39 +0200

    model tests

---
+++ src/tests/models/general.py
@@ -0,0 +1,44 @@
+import unittest
+import pandas as pd
+
+from src.models import general
+
+
+class GeneralTest(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_aggregate_cv_results(self):
+        cv_results = [
+            {'metric1': 1, 'metric2': 4},
+            {'metric1': 2, 'metric2': 5},
+            {'metric1': 3, 'metric2': 6},
+        ]
+        agg_results = general.aggregate_cv_results(cv_results)
+        self.assertIsInstance(agg_results, dict)
+        self.assertTrue('metric1' in agg_results)
+        self.assertTrue('metric2' in agg_results)
+        self.assertDictEqual(agg_results, {
+            'metric1': 2,
+            'metric2': 5,
+        })
+
+    def test_compute_regression_metrics(self):
+        y_true = pd.Series([1, 2, 3, 4])
+        y_pred = pd.Series([0.5, 1.5, 2.5, 3.5])
+        metrics = general.compute_regression_metrics(y_true, y_pred)
+        self.assertIsInstance(metrics, dict)
+        self.assertTrue('mean_squared_error' in metrics)
+        self.assertTrue('mean_absolute_percentage_error' in metrics)
+        self.assertTrue('median_absolute_error' in metrics)
+        self.assertTrue('r2_score' in metrics)
+        self.assertTrue('mean_absolute_error' in metrics)
+        self.assertTrue('root_mean_square_error' in metrics)
+
+    def test_compute_classification_metrics(self):
+        # TODO: rewrite once implemented
+        y_true = pd.Series([1, 2, 3, 4])
+        y_pred = pd.Series([0.5, 1.5, 2.5, 3.5])
+        with self.assertRaises(NotImplementedError):
+            agg_results = general.compute_classification_metrics(
+                y_true, y_pred)
