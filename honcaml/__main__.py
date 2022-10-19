ad9996197c74b6461f3cdf26f00e904d2ed7e214
Joan Erráez <joan.erraez@eurecat.org>
2022-10-19 08:19:01 +0200

    Add version as cli argument

--- honcaml/__main__.py
+++ honcaml/__main__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import argparse
+import pkg_resources
 
 from honcaml.tools import execution
+
+
+__version__ = pkg_resources.get_distribution('honcaml').version
 
 
 def cli():
@@ -9,15 +13,22 @@ def cli():
     Command-line interface for HONCAML.
     """
     parser = argparse.ArgumentParser(
+        prog="honcaml",
         description="HONCAML pipeline command-line interface.",
         usage="""
 honcaml [<args>]
 """)
 
     parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s " + __version__,
+        help='HONCAML current version')
+
+    parser.add_argument(
         "-c",
         "--config",
-        default="base_pipeline.yaml",
         type=str,
         help='YAML configuration file specifying pipeline options')
 
