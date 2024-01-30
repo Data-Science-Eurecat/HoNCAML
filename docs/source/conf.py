5bf68fd752618464d53cbf74d11aa755f597d876
Joan Erráez <joan.erraez@eurecat.org>
2024-01-30 11:37:23 +0100

    Update version to v0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 project = 'HoNCAML'
 copyright = '2023, Eurecat'
 author = 'Joan Erráez'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
